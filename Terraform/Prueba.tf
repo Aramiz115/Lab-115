9b00973665bd13d9a1b2e9451f5fc83e4842faa7
AxelAramis <axel1234mart@hotmail.com>
2023-11-03 13:51:06 -0400

    Pipe final examen

---
+++ Terraform/Prueba.tf
@@ -0,0 +1,54 @@
+provider "aws" {
+  region = "us-east-2" 
+}
+
+resource "aws_instance" "default" {
+  ami           = "ami-0e83be366243f524a" 
+  instance_type = "t2.micro"             
+  key_name      = "Prueba"
+  associate_public_ip_address = false  
+  vpc_security_group_ids = ["sg-0d953efa5f9828998"]  
+}
+	output "My_ip"{
+           value = aws_instance.default.public_ip
+           }
+           
+resource "aws_security_group" "default" {
+  name        = "allow-ssh-http"
+  description = "Security Group para SSH y HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+    ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+    ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+    }
+}
